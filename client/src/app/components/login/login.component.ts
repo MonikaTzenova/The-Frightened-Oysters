d3f053e713bc633642fe4db2c51355c7740b5e47
Simeon <petkov.spp@gmail.com>
2017-09-06 08:50:52 +0300

    update login component logic & html

--- client/src/app/components/login/login.component.ts
+++ client/src/app/components/login/login.component.ts
@@ -1,4 +1,11 @@
+import { IError } from './../../models/IError';
 import { Component, OnInit } from '@angular/core';
+import { FormGroup, FormControl, Validators } from '@angular/forms';
+import { UsersService } from './../../services/users.service';
+import { Router } from '@angular/router';
+
+import { ICookie } from './../../models/ICookie';
+import { ILoginUser } from './../../models/ILoginUser';
 
 @Component({
   selector: 'app-login',
@@ -6,10 +13,48 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./login.component.css']
 })
 export class LoginComponent implements OnInit {
+  private errorMessage: string;
 
-  constructor() { }
+  private username: FormControl;
+  private password: FormControl;
+
+  private loginForm: FormGroup;
+
+  constructor(private router: Router, private userService: UsersService) { }
 
   ngOnInit() {
+    this.buildLoginFormsData();
+  }
+
+  buildLoginFormsData() {
+    this.username = new FormControl('', [
+      Validators.required
+    ]);
+
+    this.password  = new FormControl('', [
+      Validators.required
+    ]);
+
+    this.loginForm = new FormGroup({
+      username: this.username,
+      password: this.password
+    });
+  }
+
+  login(userData: ILoginUser) {
+    const handleSuccessResponse = (cookie: ICookie) => {
+      this.errorMessage = undefined;
+      this.router.navigate(['home'], { queryParams: { 'refresh': 1 } });
+    };
+    const handleError = (error: IError) => {
+      this.errorMessage = error.message;
+      this.username.setValue('');
+      this.password.setValue('');
+      this.loginForm.markAsUntouched();
+    };
+
+    this.userService.login(userData)
+      .subscribe(handleSuccessResponse, handleError);
   }
 
 }
