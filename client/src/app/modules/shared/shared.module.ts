bb94dc868326831d7798f6738be6c57b31fa39c3
Simeon <petkov.spp@gmail.com>
2017-09-10 18:17:58 +0300

    updated share module

--- client/src/app/modules/shared/shared.module.ts
+++ client/src/app/modules/shared/shared.module.ts
@@ -6,6 +6,8 @@ import { NavBarComponent } from '../../components/nav-bar/nav-bar.component';
 import { FooterComponent } from '../../components/footer/footer.component';
 import { ListProductElementComponent } from '../../components/products/list-product-element/list-product-element.component';
 
+import { PhonePipe } from '../../pipes/phone-pipe';
+
 @NgModule({
   imports: [
     CommonModule,
@@ -14,14 +16,16 @@ import { ListProductElementComponent } from '../../components/products/list-prod
   declarations: [
     NavBarComponent,
     FooterComponent,
-    ListProductElementComponent
+    ListProductElementComponent,
+    PhonePipe
   ],
   exports: [
     NavBarComponent,
     FooterComponent,
     ListProductElementComponent,
     CommonModule,
-    RouterModule
+    RouterModule,
+    PhonePipe
   ]
 })
 export class SharedModule { }
